3da2c6260b1c3ed939335099df6d726ee4cf99af
Malachi Grant <malachimgrant@gmail.com>
2024-12-02 00:37:55 -0400

    Added plop

---
+++ plopfile.js
@@ -0,0 +1,36 @@
+export default function (plop) {
+  plop.setGenerator('new day', {
+    description: 'Creates a new day folder and initializes files',
+    prompts: [
+      {
+        type: 'input',
+        name: 'year',
+        message: 'Insert year',
+        default: new Date().getFullYear(),
+      },
+      {
+        type: 'input',
+        name: 'day',
+        message: 'Insert day',
+        default: new Date().getDay() + 1,
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/year/{{year}}/{{day}}/input.txt',
+        template: '',
+      },
+      {
+        type: 'add',
+        path: 'src/year/{{year}}/{{day}}/testInput.txt',
+        template: '__TEST_RESULTS__X__Y',
+      },
+      {
+        type: 'add',
+        path: 'src/year/{{year}}/{{day}}/index.ts',
+        templateFile: 'templates/day.hbs',
+      },
+    ],
+  });
+}
---
+++ templates/day.hbs
@@ -0,0 +1,7 @@
+export function part1(lines: string[]) {
+    return 0;
+}
+
+export function part2(lines: string[]) {
+    return 0;
+}
